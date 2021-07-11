ad202cbc239792af3c4bc6e13f85ff1c8fe11a73
Gargi Chaurasia <gargi.0901@gmail.com>
2021-07-11 22:10:15 +0530

    Resolved header slider nextSlide issue

--- js/index.js
+++ js/index.js
@@ -14,37 +14,37 @@ const countBoxContainer = document.querySelector(".count-boxes");
 
 //creating boxes function
 const createBoxes = function () {
-  slides.forEach((_, i) => {
-    countBoxContainer.insertAdjacentHTML(
-      "beforeend",
-      `<button class = "count-boxes__box" data-slide = "${i}">0${
-        i + 1
-      }</button>`
-    );
-  });
+	slides.forEach((_, i) => {
+		countBoxContainer.insertAdjacentHTML(
+			"beforeend",
+			`<button class = "count-boxes__box" data-slide = "${i}">0${
+				i + 1
+			}</button>`
+		);
+	});
 };
 createBoxes();
 
 //Activating boxes
 
 const activateBoxes = function (slide) {
-  const allBoxes = document.querySelectorAll(".count-boxes__box");
-  allBoxes.forEach((box) => {
-    box.classList.remove("count-boxes__box--active");
-  });
-  document
-    .querySelector(`.count-boxes__box[data-slide="${slide}"]`)
-    .classList.add("count-boxes__box--active");
+	const allBoxes = document.querySelectorAll(".count-boxes__box");
+	allBoxes.forEach((box) => {
+		box.classList.remove("count-boxes__box--active");
+	});
+	document
+		.querySelector(`.count-boxes__box[data-slide="${slide}"]`)
+		.classList.add("count-boxes__box--active");
 };
 
 // Go to slide no 'slide'
 const goToSlide = function (slide) {
-  slides.forEach((s, i) => {
-    s.style.transform = `translateX(${100 * (i - slide)}%)`;
-  });
-
-  // activateDots(slide);
-  activateBoxes(slide);
+	slides[slide].style.zIndex = 10;
+	slides.forEach((s, i) => {
+		s.style.transform = `translateX(${100 * (i - slide)}%)`;
+	});
+	// activateDots(slide);
+	activateBoxes(slide);
 };
 
 goToSlide(0);
@@ -53,24 +53,23 @@ goToSlide(0);
 
 //For going to next slide
 const nextSlide = function () {
-  if (currentSlide === maxSlide - 1) {
-    currentSlide = 0;
-  } else {
-    currentSlide++;
-  }
-
-  goToSlide(currentSlide);
+	if (currentSlide >= maxSlide - 1) {
+		currentSlide = 0;
+	} else {
+		currentSlide++;
+	}
+	goToSlide(currentSlide);
 };
 
 //for going to prev slide
 const prevSlide = function () {
-  if (currentSlide === 0) {
-    currentSlide = maxSlide - 1;
-  } else {
-    currentSlide--;
-  }
+	if (currentSlide === 0) {
+		currentSlide = maxSlide - 1;
+	} else {
+		currentSlide--;
+	}
 
-  goToSlide(currentSlide);
+	goToSlide(currentSlide);
 };
 
 /** Event handlers*/
@@ -83,24 +82,25 @@ btnLeft.addEventListener("click", prevSlide);
 // handling slider using key arrow left and key arrow right
 
 document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowLeft") {
-    prevSlide();
-  }
-  if (e.key === "ArrowRight") {
-    nextSlide();
-  }
+	if (e.key === "ArrowLeft") {
+		prevSlide();
+	}
+	if (e.key === "ArrowRight") {
+		nextSlide();
+	}
 });
 
 //For box functionality
 
 countBoxContainer.addEventListener("click", function (e) {
-  if (e.target.classList.contains("count-boxes__box")) {
-    const { slide } = e.target.dataset;
-    goToSlide(slide);
-  }
+	if (e.target.classList.contains("count-boxes__box")) {
+		const { slide } = e.target.dataset;
+		goToSlide(slide);
+		currentSlide = slide;
+	}
 });
 
-setInterval(nextSlide, 6000);
+const timer = setInterval(nextSlide, 6000);
 
 // For testimonial slider
 const slider2 = document.querySelector(".slider-2");
@@ -113,34 +113,34 @@ let currentTestimonial = 0;
 
 // Go to slide no 'slide'
 const goToTestimonial = function (t) {
-  testimonials.forEach((s, i) => {
-    s.style.transform = `translateX(${100 * (i - t)}%)`;
-  });
-  // activateDots(slide);
+	testimonials.forEach((s, i) => {
+		s.style.transform = `translateX(${100 * (i - t)}%)`;
+	});
+	// activateDots(slide);
 };
 
 goToTestimonial(0);
 
 //For going to next slide
 const nextTestimonial = function () {
-  if (currentTestimonial === numTestimonials - 1) {
-    currentTestimonial = 0;
-  } else {
-    currentTestimonial++;
-  }
+	if (currentTestimonial === numTestimonials - 1) {
+		currentTestimonial = 0;
+	} else {
+		currentTestimonial++;
+	}
 
-  goToTestimonial(currentTestimonial);
+	goToTestimonial(currentTestimonial);
 };
 
 //for going to prev slide
 const prevTestimonial = function () {
-  if (currentTestimonial === 0) {
-    currentTestimonial = numTestimonials - 1;
-  } else {
-    currentTestimonial--;
-  }
+	if (currentTestimonial === 0) {
+		currentTestimonial = numTestimonials - 1;
+	} else {
+		currentTestimonial--;
+	}
 
-  goToTestimonial(currentTestimonial);
+	goToTestimonial(currentTestimonial);
 };
 
 /** Event handlers*/
